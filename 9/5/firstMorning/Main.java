c1e47eba9fb95984829b380d9c324ffdf7d08fa1
badgelatte <saucy5939@gmail.com>
2023-09-05 13:02:17 +0900

    파일 갈래 하나 추가

--- 9/5/firstMorning/Main.java
+++ 9/5/firstMorning/Main.java
@@ -1,4 +1,6 @@
-public class Main    {
+// package firstMorning;
+
+public class Main {
 
     public static void main(String[] args) {
         int[] input = {0, 3, 2, 624, 339, 13, 45232};
@@ -6,6 +8,13 @@ public class Main    {
 
         System.out.printf("Out of %d Cases, %1.0f were correct", input.length, test(input, output));
     }
+    
+    /**
+     * 
+     * @param input
+     * @param output
+     * @return
+     */
 
     public static double test(int[] input, String[] output) {
         double score = 0;
@@ -14,7 +23,7 @@ public class Main    {
             System.out.printf("Case %d %n", i);
 
             String correct = output[i];
-            System.out.printf("Input : %d \t -> Expected : %s %n",input[i], output[i]);
+            System.out.printf("Input : %d \t -> Expected : %s %n", input[i], output[i]);
 
             String str = solution(input[i]);
             System.out.printf("Your Answer : %s %n", str);
@@ -27,32 +36,34 @@ public class Main    {
         return score;
     }
 
-    public static String solution(int input){   //내가 한 거
+    public static String solution(int input) { // 내가 한 거
         int tenToTwo;
         String output = "";
 
-        if(input == 0){
+        if (input == 0) {
             output = "0";
             return output;
         }
 
-        for(int i = 0; input >0;i++){
-            
+        for (int i = 0; input > 0; i++) {
+
             tenToTwo = input % 2;
             input = input / 2;
             output = Integer.toString(tenToTwo) + output;
         }
 
         return output;
-        
+
     }
 
-    public static String solution2(int input){  // 정답지
+    public static String solution2(int input) { // 정답지
         String answer = "";
         do {
             answer = (input % 2) + answer;
             input /= 2;
         } while (input > 0);
+
+
 
         return answer;
     }
