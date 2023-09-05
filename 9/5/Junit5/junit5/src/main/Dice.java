78463e3025bcce57f222473d391aaba12ca12c07
badgelatte <saucy5939@gmail.com>
2023-09-05 15:22:50 +0900

    junit dice 문구까지 추가

--- 9/5/Junit5/junit5/src/main/Dice.java
+++ 9/5/Junit5/junit5/src/main/Dice.java
@@ -4,8 +4,8 @@ public class Dice {
     private int number; // private - 캡슐화 지키기 위해 달기! && 자동화 땜시 getter setter 무조건 넣으면 캡슐 깨짐
 
     public Dice(int number){
-        if (number <= 0 && number > 6) 
-            throw new IllegalArgumentException("1미만 6 초과 주사위는 생성할 수 없습니다.");
+        if (number <= 0 || number > 6) 
+            throw new IllegalArgumentException("1 미만 6 초과 주사위는 생성할 수 없습니다.");
         this.number = number;
     }
 
--- 9/5/Junit5/junit5/src/test/DiceJunitTest.java
+++ 9/5/Junit5/junit5/src/test/DiceJunitTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,15 @@ class DiceJunitTest {
         Dice second = new Dice (2);
 
         assertEquals(DiceCalculator.addDice(first, second), 8);
+    }
+
+
+    @Test
+    @DisplayName("음수 값의 주사위를 생성할 수 없을 때, IllegalArgumentException을 던짐.")
+    void negativeDice_throwIllegalArgumentException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Dice(-1));
+        // ...?
+        assertEquals(exception.getMessage(), "1 미만 6 초과 주사위는 생성할 수 없습니다.");
+        // Dice.java의 생성자 속 " " 안의 문구가 달라지면 에러가 뜬다
     }
 }
