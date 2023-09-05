001293eaa310da7953e85faadca702ec2d498c65
badgelatte <saucy5939@gmail.com>
2023-09-05 15:14:39 +0900

    junit @test 사용

---
+++ 9/5/Junit5/junit5/src/test/DiceJunitTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import main.Dice;
+import main.DiceCalculator;
+
+class DiceJunitTest {
+    @Test   // Test가 2개인데 junipter api 사용해야한다 
+    //@DisplayName("addDice 성공")
+    void addDice() {
+        Dice first = new Dice(6);
+        Dice second = new Dice (2);
+
+        assertEquals(DiceCalculator.addDice(first, second), 8);
+    }
+}
