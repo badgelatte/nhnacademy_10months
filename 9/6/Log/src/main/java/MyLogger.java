dc2e88dd73639594658857c97e2a37399bdda2f6
badgelatte <saucy5939@gmail.com>
2023-09-06 10:40:08 +0900

    logger, logback까지 실행

--- 9/6/Log/src/main/java/MyLogger.java
+++ 9/6/Log/src/main/java/MyLogger.java
@@ -1,3 +1,5 @@
+package main.java;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -5,7 +7,7 @@ public class MyLogger {
     private final static Logger logger = Logger.getLogger("myLogger");
 
     public static Logger getLogger() {
-        logger.setLevel(Level.INFO);    // 여기에 INFO 대신 SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
+        logger.setLevel(Level.INFO);
         return logger;
     }
 }
--- 9/6/Log/src/test/java/LoggerTest.java
+++ 9/6/Log/src/test/java/LoggerTest.java
@@ -1,19 +1,22 @@
+package test.java;
+
+import main.java.MyLogger;
+
 public class LoggerTest {
     public static int sum(int a, int b) {
         MyLogger.getLogger().info("덧셈 수행 : " + a + " + " + b);
-        //info 대신 다른 걸 설정하면 다르게 나온다 -> ex. warning 설정 시 warning위로는 나오지만 아래는 안나온다
         return a + b;
     }
 
-    public static double divide (int a, int b) {
-        if( b== 0 ) {
+    public static double divide(int a, int b) {
+        if (b == 0) {
             MyLogger.getLogger().warning("나눗셈 에러 발생!");
             throw new ArithmeticException("0으로 나눌 수 없습니다.");
         }
         return a / b;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a = 1;
         int b = 2;
         System.out.println(sum(a, b));
