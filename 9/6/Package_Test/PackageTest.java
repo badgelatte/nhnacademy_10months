314f50794cd39d0e85eb9901981a69b13048b326
badgelatte <saucy5939@gmail.com>
2023-09-06 13:42:42 +0900

    package 지정 2번째 방법

--- 9/6/Package_Test/PackageTest.java
+++ 9/6/Package_Test/PackageTest.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class PackageTest{
     public static void main(String[] args) {
         java.util.List<String> list1 = new java.util.LinkedList<>(null);
+        List<Integer> list2 = new ArrayList<>();
     }
 }
