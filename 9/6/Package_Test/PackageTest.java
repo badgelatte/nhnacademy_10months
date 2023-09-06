b21c9426ff615c882931e00420a0a07dce5efb0c
badgelatte <saucy5939@gmail.com>
2023-09-06 13:47:01 +0900

    package 사용 방법

--- 9/6/Package_Test/PackageTest.java
+++ 9/6/Package_Test/PackageTest.java
@@ -1,10 +1,11 @@
-import java.util.*;
-import static java.util.Collections.*;
+import java.util.*;     // 2
+import static java.util.Collections.*;      // 3
 
 public class PackageTest{
     public static void main(String[] args) {
-        java.util.List<String> list1 = new java.util.LinkedList<>(null);
-        List<Integer> list2 = new ArrayList<>();
-        Collections.sort(list1, null);
+        java.util.List<String> list1 = new java.util.LinkedList<>(null); // 1
+        List<Integer> list2 = new ArrayList<>();    // 2
+        Collections.sort(list1, null);      // 3
+        sort(list2, null);      // 3
     }
 }
