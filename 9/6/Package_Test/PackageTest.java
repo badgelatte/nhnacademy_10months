d73cc5f4af75d3d3875112352cdf2b7ee6e5d364
badgelatte <saucy5939@gmail.com>
2023-09-06 13:48:16 +0900

    잘못 알고 있던 package 수정

--- 9/6/Package_Test/PackageTest.java
+++ 9/6/Package_Test/PackageTest.java
@@ -5,7 +5,7 @@ public class PackageTest{
     public static void main(String[] args) {
         java.util.List<String> list1 = new java.util.LinkedList<>(null); // 1
         List<Integer> list2 = new ArrayList<>();    // 2
-        Collections.sort(list1, null);      // 3
+        Collections.sort(list1, null);      // 2
         sort(list2, null);      // 3
     }
 }
