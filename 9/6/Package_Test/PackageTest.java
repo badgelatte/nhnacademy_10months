135eba5d94fba254d44bac130755ca9383feeee6
badgelatte <saucy5939@gmail.com>
2023-09-06 13:45:14 +0900

    package 3번째 지정

--- 9/6/Package_Test/PackageTest.java
+++ 9/6/Package_Test/PackageTest.java
@@ -1,8 +1,10 @@
 import java.util.*;
+import static java.util.Collections.*;
 
 public class PackageTest{
     public static void main(String[] args) {
         java.util.List<String> list1 = new java.util.LinkedList<>(null);
         List<Integer> list2 = new ArrayList<>();
+        Collections.sort(list1, null);
     }
 }
