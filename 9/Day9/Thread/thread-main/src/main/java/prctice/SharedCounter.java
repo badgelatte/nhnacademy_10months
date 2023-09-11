775ed9c7bd0b29b8595dcbb36dfdd7e09bcd21a1
badgelatte <saucy5939@gmail.com>
2023-09-11 16:40:12 +0900

    6.synchronization_index 속 Exam-01

---
+++ 9/Day9/Thread/thread-main/src/main/java/prctice/SharedCounter.java
@@ -0,0 +1,55 @@
+package prctice;
+
+public class SharedCounter implements Runnable{
+    Thread thread;
+    int count = 0;
+    int maxCount;
+    SharedCount sharedCount;    // 공유 카운터
+
+    public void SharedCounter(String name, int maxCount, SharedCount sharedCount){
+        thread = new Thread(this, name);
+        this.maxCount = maxCount;
+        this.sharedCount = sharedCount;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void start() {
+        //내가 가진 스레드를 실행
+        thread.start();
+    }
+
+    public void stop() {
+        thread.interrupt();
+    }
+
+    @Override
+    public void run() {
+        while(!Thread.currentThread().isInterrupted() && (count < maxCount)) {
+            count++;
+            sharedCount.increment();
+        }
+    }
+
+    public boolean isAlive() {
+        return thread.isAlive();
+    }
+
+    public static void main(String[] args) {
+        SharedCount sharedCount = new SharedCount();
+        SharedCounter counter1 = new SharedCounter("counter1", 10000, sharedCount);
+        SharedCounter counter2 = new SharedCounter("counter2", 10000, sharedCount);
+
+        counter1.start();
+        counter2.start();
+
+        while(counter1.isAlive() || counter2.isAlive());
+
+        System.out.println("counter1: " + counter1.getCount());
+        System.out.println("counter2: " + counter2.getCount());
+        System.out.println("ShareCount: " +sharedCount.getCount());
+    }
+
+}
