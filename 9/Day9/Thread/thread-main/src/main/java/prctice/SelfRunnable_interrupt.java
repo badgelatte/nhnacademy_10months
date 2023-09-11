34fb3be46504dc3d2ac9720ba7b46a064beae43d
badgelatte <saucy5939@gmail.com>
2023-09-11 16:39:30 +0900

    runnable

--- 9/Day9/Thread/thread-main/src/main/java/prctice/SelfRunnable_interrupt.java
+++ 9/Day9/Thread/thread-main/src/main/java/prctice/SelfRunnable_interrupt.java
@@ -28,7 +28,6 @@ public class SelfRunnable_interrupt implements Runnable{ // made by teacher
 
     @Override
     public void run() {
-        running = true;     // 상태가 시작으로 바꿈
         thread = Thread.currentThread();
 
         // Thread.currentThread().isInterrupted() - interrupted가 떳나 안떳나만 확인하고 초기화는 하지 않는다
@@ -44,6 +43,7 @@ public class SelfRunnable_interrupt implements Runnable{ // made by teacher
                 // InterruptedException ignore 이걸 빼먹으면 compile 오류 뜬다
                 // Thread에 interrupt가 발생하였다고, 과정을 종료할 필요는 없다. Interrupt 발생 후 처리 과정은 다양하게 활용할 수 있다
                 // -> flag를 넣을 수도 있고 다른 방법도 많다
+                System.out.println(Thread.currentThread().isInterrupted()); // waiting 상태일때 flase로 나온다
                 Thread.currentThread().interrupt();
                 // interrupt에 setting을 해준다
             }
@@ -64,5 +64,6 @@ public class SelfRunnable_interrupt implements Runnable{ // made by teacher
 
         Thread.sleep(3000);
         counter1.stop();
+        System.out.println("Finished");
     }
 }
