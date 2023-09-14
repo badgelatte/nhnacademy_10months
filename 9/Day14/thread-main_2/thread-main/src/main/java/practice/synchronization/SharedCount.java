eea3155fc13887049f0b585459414d991deaab26
badgelatte <saucy5939@gmail.com>
2023-09-14 15:20:22 +0900

    state까지 다시 만들어봄

--- 9/Day14/thread-main_2/thread-main/src/main/java/practice/synchronization/SharedCount.java
+++ 9/Day14/thread-main_2/thread-main/src/main/java/practice/synchronization/SharedCount.java
@@ -15,7 +15,12 @@ public class SharedCount {
     }
 
     public void increment() {
-        setCount(getCount() + 1);
+        count++;
+        // setCount(getCount() + 1);
+        try{
+            Thread.sleep(100);  // 들어가서 지 할일하고 주어진 시간만큼 자는 거 
+        } catch(InterruptedException e){
+        }
     }
 
 }   
