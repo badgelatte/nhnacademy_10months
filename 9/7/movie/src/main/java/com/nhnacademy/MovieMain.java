0f98cc1d2bb234ca8a869c98dc10d3a660bafe10
badgelatte <saucy5939@gmail.com>
2023-09-14 14:36:58 +0900

    movie jar파일 다시 만들어봄

--- 9/7/movie/src/main/java/com/nhnacademy/MovieMain.java
+++ 9/7/movie/src/main/java/com/nhnacademy/MovieMain.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class MovieMain {
 
   public static void main(String[] args) {
-    //MovieParser movieParser = new BasicMovieParser(); // Basic
-    MovieParser movieParser = new ApacheCommonsCsvMovieParser();    // Apache
+    MovieParser movieParser = new BasicMovieParser(); // Basic
+    // MovieParser movieParser = new ApacheCommonsCsvMovieParser();    // Apache
 
     try {
       List<Movie> movieList = movieParser.parse("movies.csv");
