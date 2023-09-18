f0f164dcae324211a41564246e48d67209b9d7d0
badgelatte <saucy5939@gmail.com>
2023-09-18 13:07:12 +0900

    설명 추가

--- 9/Day18/network-programming/src/main/java/com/nhnacademy/Quiz02_tryWithResources.java
+++ 9/Day18/network-programming/src/main/java/com/nhnacademy/Quiz02_tryWithResources.java
@@ -20,6 +20,7 @@ public class Quiz02_tryWithResources {
     public static void main(String[] args) {
         // 포트 돌면서 스캔
         for(int port = 1; port < 65536; port++ ) { 
+            // for문 안하면 socket try문을 빠져나가서 socket을 닫아 줄 수 없다
             try{
                 Socket socket = new Socket("localhost", port);
                 System.out.println(port +"가 열려있습니다.");
