4d193857721db313bfa9a424180d2cada77fcc70
badgelatte <saucy5939@gmail.com>
2023-09-26 17:49:11 +0900

    수석님과 함께

---
+++ 9/Day26/homework/scurl/src/main/java/com/nhnacademy/InvalidMethodException.java
@@ -0,0 +1,11 @@
+package com.nhnacademy;
+
+public class InvalidMethodException extends RuntimeException{
+    public InvalidMethodException() {
+        super();
+    }
+
+    public InvalidMethodException(String message) {
+        message = new String();
+    }
+}
--- 9/Day26/homework/scurl/src/main/java/com/nhnacademy/SimpleSucrl.java
+++ 9/Day26/homework/scurl/src/main/java/com/nhnacademy/SimpleSucrl.java
@@ -1,5 +1,7 @@
 package com.nhnacademy;
 
+import java.net.Socket;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -9,7 +11,12 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class SimpleSucrl {
-Options options;
+    Options options;
+    String url = "";
+    String host = "localhost";
+    int port = 80;
+    String version = ";"
+    static String method = "GET";  // 아무것도 안하면 GET 출력
     public SimpleSucrl() {
         options = new Options();        
         
@@ -20,7 +27,19 @@ Options options;
         options.addOption("L", false, "서버 응답이 30X 계열이면 다음 응답을 따라간다.");
         options.addOption(Option.builder("F").hasArgs().argName("name=conntet").desc("multipart/form-data를 구성하여 전송한다. content 부분에 @filename을 사용할 수 있다.").build());
         options.addOption("?", false, "사용법을 출력한다");
+
     }
+    
+    
+    public void run() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%s %s %s", method, url, version));
+
+        Socket socket= new Socket(host, port);
+        socket.getOutputStream().write(...);
+    }
+
+
     public void showHelp() {
         HelpFormatter formatter = new HelpFormatter();
         formatter.setOptionComparator(null);
@@ -29,18 +48,64 @@ Options options;
 
     public CommandLine setOption(String[] args) throws ParseException{
             CommandLineParser parser = new DefaultParser();
+            CommandLine commandLine = parser.parse(options, args);
+
+            if(commandLine.hasOption("?")){
+                    showHelp();
+                    System.exit(0);
+                }
+            if(commandLine.hasOption("X")) {
+                    method = commandLine.getOptionValue("X");
+        
+                    if(!(method.equalsIgnoreCase("GET")) || !(method.equalsIgnoreCase("POST")) || !(method.equalsIgnoreCase(""))){
+                        throw new InvalidMethodException("Method 지정 잘못되었습니다.[GET, PUT, POST] :" + method);
+                    }
+        
+                }
+            if(commandLine.getArgs().length == 0) {
+                throw new InvalidURLException();
+            }
+            url = commandLine.getArgs()[0];
+        
+            // //로 자르고 그다음은 /로 자르고 그다음 :로 자른다
+            url.split("://")[1].split("/");
+            String [] splitURL = url.split("://");
+            if(splitURL.length != 2) {
+        
+            }
+        
+            //
+            String [] field = splitURL.split("/");
+            if(field[0].contains(":")) {
+                //있으면 나누고 
+                host = field[0].split(":")[0];
+                port = Integer.parseInt(field[0].split(":")[1]);
+            }
+            field[0].split(":");
+
         return parser.parse(options, args);
     }
+
+    public void setOption() {
+        
+    }
+
 
     public static void main( String[] args ){
         SimpleSucrl scurl = new SimpleSucrl();
         
         try {
-            CommandLine commandLine = scurl.setOption(args);
-            if(commandLine.hasOption("?")){
-                scurl.showHelp();
-            }
+            // 
+            
+            scurl.setOption();
+            scurl.run();
+
+
         } catch (ParseException e) {
+            scurl.showHelp();
+        } catch (InvalidMethodException e) {
+            scurl.showHelp();
+        } catch (InvalidURLException e) {
             scurl.showHelp();
         }
     }
