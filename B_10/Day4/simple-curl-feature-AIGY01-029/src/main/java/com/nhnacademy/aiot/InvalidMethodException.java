efb5c87a7200db1e539d3dfd55a590ecb8699e90
badgelatte <saucy5939@gmail.com>
2023-10-04 09:42:08 +0900

    .

---
+++ B_10/Day4/simple-curl-feature-AIGY01-029/src/main/java/com/nhnacademy/aiot/InvalidMethodException.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.aiot;
+
+public class InvalidMethodException extends RuntimeException{
+    // public InvalidMethodException() {
+    //     super();
+    // }
+
+    // public InvalidMethodException(String message) {
+    //     // message = new String();
+    //     System.out.println(message);
+    // }
+}
