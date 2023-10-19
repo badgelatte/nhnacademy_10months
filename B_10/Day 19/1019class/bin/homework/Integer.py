b01ef1c41d249145bceab23af0daf80286c9a5e1
badgelatte <saucy5939@gmail.com>
2023-10-19 22:30:17 +0900

    숙제

---
+++ B_10/Day 19/1019class/bin/homework/Integer.py
@@ -0,0 +1,15 @@
+class Integer(object):
+    def __init__(self, x):
+        self.x = x
+    
+    def toString(self):
+        return str(self, x)
+
+    @staticmethod    
+    def parse(self, offer):
+        
+        return offer
+
+i = Integer(3)
+print(i.toString)
+Integer.parse('5')
