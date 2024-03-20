7a1dc03d21ad2cdf08b6cd2c60e82b6a0101818d
badgelatte <saucy5939@gmail.com>
2024-03-20 13:33:47 +0900

    로그인을 Promise를 사용하여 만들기

--- H_3/Day20/javascript/shoppingCart/api-xhr.js
+++ H_3/Day20/javascript/shoppingCart/api-xhr.js
@@ -91,97 +91,84 @@ window.addEventListener("DOMContentLoaded",function(){
     });
 
     function getCartItems(userId, cartId, viewItems) {
+        const executor = function(resolve, reject) {
+            // 이렇게 작성시 굳이 +를 붙여가며 사용하지 않아도 된다
+            const requestUrl = `${SERVER_URL}/api/nhnmart/shopping-cart/${cartId}`;
+            
+            xhr.addEventListener("load",function(){
+                if(this.status===200) {
+                    console.log(JSON.stringify(this.response));
+                    // 모든 걸 한번에 받음
+                    const items = this.response;
+                
+                    resolve(items);
+                }
+                // 에러를 잡고 싶다면 이렇게 작성하면 되는데 실상 잡기 힘들다
+                // getCartItems이라는 collback 함수를 사용하기때문에
+                //  또한, 비동기 방식으로 돌아가기 때문에 잡기 어렵다
+                else {
+                    reject(new Error(this.status));
+                }
+            });
+        
+            xhr.addEventListener("error", function(){
+                // throw new Error("error"); -> 이렇게 잡기 어렵다
+                // error 처리 ui 만들어야한다
+            });
+        
+            // 초기화
+            xhr.open("GET", requestUrl);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.setRequestHeader("X-USER-ID",userId);
+            xhr.responseType="json";
+        
+            // 호출
+            xhr.send('');
+            const promise  = new Promise(executor);
+            return promise;
+        }
+
+        const promise = new Promise();
+
         const xhr = new XMLHttpRequest();
 
-        // 이렇게 작성시 굳이 +를 붙여가며 사용하지 않아도 된다
-        const requestUrl = `${SERVER_URL}/api/nhnmart/shopping-cart/${cartId}`;
-
-        xhr.addEventListener("load",function(){
-            if(this.status===200) {
-                console.log(JSON.stringify(this.response));
-                // 모든 걸 한번에 받음
-                const items = this.response;
-
-                // ui 처리
-                viewItems(items);
-            }
-            // 에러를 잡고 싶다면 이렇게 작성하면 되는데 실상 잡기 힘들다
-            // getCartItems이라는 collback 함수를 사용하기때문에
-            //  또한, 비동기 방식으로 돌아가기 때문에 잡기 어렵다
-            else {
-                throw new Error(this.status);
-            }
-        });
-
-        xhr.addEventListener("error", function(){
-            // throw new Error("error"); -> 이렇게 잡기 어렵다
-            // error 처리 ui 만들어야한다
-        });
-
-        // 초기화
-        xhr.open("GET", requestUrl);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-USER-ID",userId);
-        xhr.responseType="json";
-
-        // 호출
-        xhr.send();
+        
     }
 
     // 로그인 호출 -> 서버 호출 + ui 처리까지 하고 있다
     function doLogin(userId, userPassword, loginSuccess ) {
-        const xhr = new XMLHttpRequest();
-        const requestUrl = SERVER_URL + "/api/users/login";
-        
-        // const user = {
-            //     'userId' : userId,
-            //     'userPassword': userPassword0
-            // }
-            // 요거ㅓ 대신
-            
+        const executor = function(resolve, reject){
+            const xhr = new XMLHttpRequest();
+            const requestUrl = SERVER_URL + "/api/users/login";
+
             const user = {
                 userId,
                 userPassword
             }
-            
-            // 성공적으로 다 끝나면 실행
-            xhr.addEventListener("load", function(){
-                if(this.status === 200) {
-                    // 로그인 성공 -> json -> Object 순서로 변환
-                    // 초기화 때 json으로 받는다해서 변경해줘야한다
-                    const userInfo = this.response;
-                    loginSuccess(userInfo)
 
-                    /* // login success 처리..
-                    console.log("userInfo", JSON.stringify(userInfo));
-                    //display:block -> display:none;
-                    // block이 보여주는 거고 none이 아무것도 안 보여주는거
-                    const loginWrapper = document.querySelector('#login-wrapper');
-                    loginWrapper.setAttribute("style", "display:none;");
+                // 성공적으로 다 끝나면 실행
+                xhr.addEventListener("load", function(){
+                    if(this.status === 200) {
+                        // 로그인 성공 -> json -> Object 순서로 변환
+                        // 초기화 때 json으로 받는다해서 변경해줘야한다
+                        const userInfo = this.response;
+                        loginSuccess(userInfo)
+                    }
+                    else {
+                        reject(new Error("login Fail"));
+                    }
+                });
 
-                    //display:none -> display:block;
-                    const loginSuccess = document.querySelector("#login-success");
-                    loginSuccess.setAttribute("style", "display:block;");
+                // 초기화
+                xhr.open("POST",requestUrl);
+                xhr.setRequestHeader("Content-Type","application/json");
+                xhr.responseType = "json";
 
-                    // 로그인 성공 후 값 표시
-                    // div - ul - li 속 id값을 넣으면 된다 
-                    const loginUserId = document.getElementById("login-userId");
-                    const loginUserName = document.getElementById("login-userName");
-                    const loginCartId = document.getElementById("login-cartId");
-
-                    // span 안에 값 넣기 설정
-                    loginUserId.innerText = userInfo.userId;
-                    loginUserName.innerText = userInfo.userName;
-                    loginCartId.innerText = userInfo.cartId; */
-                }
-            });
-            
-            // 초기화
-            xhr.open("POST",requestUrl);
-            xhr.setRequestHeader("Content-Type","application/json");
-            xhr.responseType = "json";
-            
-            // 전송 -> api 호출
-            xhr.send(JSON.stringify(user)); // json 문자열로 전송
+                // 전송 -> api 호출
+                xhr.send(JSON.stringify(user)); // json 문자열로 전송
+                const promise = new Promise(executor);
+                return promise;
+            }
+        
         }
     });
