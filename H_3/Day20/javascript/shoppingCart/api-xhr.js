0d910cebd3bff8535e36d9919c5ea8c47b700607
badgelatte <saucy5939@gmail.com>
2024-03-20 11:30:41 +0900

    장바구니에 아이템 불러온거 보여주기

--- H_3/Day20/javascript/shoppingCart/api-xhr.js
+++ H_3/Day20/javascript/shoppingCart/api-xhr.js
@@ -2,7 +2,7 @@
 const SERVER_URL = "http://133.186.241.167:8100";
 
 window.addEventListener("DOMContentLoaded",function(){
-    'use strict'; // ->
+    'use strict'; 
 
     const loginForm = document.querySelector("#login-form");
 
@@ -56,8 +56,77 @@ window.addEventListener("DOMContentLoaded",function(){
             loginUserId.innerText = userInfo.userId;
             loginUserName.innerText = userInfo.userName;
             loginCartId.innerText = userInfo.cartId;
+
+            // getCarts(userInfo.userId, userInfo.cartId);
+            // 장바구니 호출 api
+            getCartItems(userInfo.userId, userInfo.cartId, function(items){
+                // table에 id 있으니까 찾아와서 그 안의 tbody를 byTagName으로 접근한다
+                const cartTable = document.querySelector("#cart-table");
+                const tbody = cartTable.getElementsByTagName("tbody")[0];
+                // 이렇게 해도 되고
+                // for(let i = 0; i<items.length; i++) {}
+
+                //이런 방식으로 해도 된다
+                for(let item of items) {
+                    // html tag를 만든 것이다
+                    const tr = document.createElement("tr");
+                    const td1 = document.createElement("td");
+                    const td2 = document.createElement("td");
+                    const td3 = document.createElement("td");
+                    const td4 = document.createElement("td");
+                    const td5 = document.createElement("td");
+
+                    td1.innerText = item.productId;
+                    td2.innerText = item.name;
+                    td3.innerText = item.price;
+                    td4.innerText = item.amount;
+                    td5.innerText = item.totalPrice;
+
+                    // 만든 tr, td를 tbody > tr > td 순으로 넣어준다
+                    tr.append(td1, td2, td3, td4, td5);
+                    tbody.append(tr);
+                }
+            });
         });
     });
+
+    function getCartItems(userId, cartId, viewItems) {
+        const xhr = new XMLHttpRequest();
+
+        // 이렇게 작성시 굳이 +를 붙여가며 사용하지 않아도 된다
+        const requestUrl = `${SERVER_URL}/api/nhnmart/shopping-cart/${cartId}`;
+
+        xhr.addEventListener("load",function(){
+            if(this.status===200) {
+                console.log(JSON.stringify(this.response));
+                // 모든 걸 한번에 받음
+                const items = this.response;
+
+                // ui 처리
+                viewItems(items);
+            }
+            // 에러를 잡고 싶다면 이렇게 작성하면 되는데 실상 잡기 힘들다
+            // getCartItems이라는 collback 함수를 사용하기때문에
+            //  또한, 비동기 방식으로 돌아가기 때문에 잡기 어렵다
+            else {
+                throw new Error(this.status);
+            }
+        });
+
+        xhr.addEventListener("error", function(){
+            // throw new Error("error"); -> 이렇게 잡기 어렵다
+            // error 처리 ui 만들어야한다
+        });
+
+        // 초기화
+        xhr.open("GET", requestUrl);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("X-USER-ID",userId);
+        xhr.responseType="json";
+
+        // 호출
+        xhr.send();
+    }
 
     // 로그인 호출 -> 서버 호출 + ui 처리까지 하고 있다
     function doLogin(userId, userPassword, loginSuccess ) {
