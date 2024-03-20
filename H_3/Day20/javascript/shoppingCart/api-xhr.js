cbc6b2d3654df201ccb8b06f53292f1c869592e1
badgelatte <saucy5939@gmail.com>
2024-03-20 10:40:19 +0900

    로그인 화면 구현 따로 메소드로 빼기

--- H_3/Day20/javascript/shoppingCart/api-xhr.js
+++ H_3/Day20/javascript/shoppingCart/api-xhr.js
@@ -33,35 +33,8 @@ window.addEventListener("DOMContentLoaded",function(){
         // const userPassword = document.querySelector("#userPassword").value;
         const userId = loginForm['userId'].value;
         const userPassword = loginForm['userPassword'].value;
-        
-        //login api 호출
-        doLogin(userId,userPassword);
-    });
-
-    // 로그인 호출 -> 서버 호출 + ui 처리까지 하고 있다ㄴ
-    function doLogin(userId, userPassword) {
-        const xhr = new XMLHttpRequest();
-        const requestUrl = SERVER_URL + "/api/users/login";
-        
-        // const user = {
-            //     'userId' : userId,
-            //     'userPassword': userPassword0
-            // }
-            // 요거ㅓ 대신
-            
-            const user = {
-                userId,
-                userPassword
-            }
-            
-            // 성공적으로 다 끝나면 실행
-            xhr.addEventListener("load", function(){
-                if(this.status === 200) {
-                    // 로그인 성공 -> json -> Object 순서로 변환
-                    // 초기화 때 json으로 받는다해서 변경해줘야한다
-                    const userInfo = this.response;
-                    
-                    // login success 처리..
+        const loginSuccess = function(userInfo){
+            // login success 처리..
                     console.log("userInfo", JSON.stringify(userInfo));
                     //display:block -> display:none;
                     // block이 보여주는 거고 none이 아무것도 안 보여주는거
@@ -82,6 +55,57 @@ window.addEventListener("DOMContentLoaded",function(){
                     loginUserId.innerText = userInfo.userId;
                     loginUserName.innerText = userInfo.userName;
                     loginCartId.innerText = userInfo.cartId;
+        };
+        
+        //login api 호출
+        doLogin(userId,userPassword, loginSuccess);
+    });
+
+    // 로그인 호출 -> 서버 호출 + ui 처리까지 하고 있다
+    function doLogin(userId, userPassword, loginSuccess ) {
+        const xhr = new XMLHttpRequest();
+        const requestUrl = SERVER_URL + "/api/users/login";
+        
+        // const user = {
+            //     'userId' : userId,
+            //     'userPassword': userPassword0
+            // }
+            // 요거ㅓ 대신
+            
+            const user = {
+                userId,
+                userPassword
+            }
+            
+            // 성공적으로 다 끝나면 실행
+            xhr.addEventListener("load", function(){
+                if(this.status === 200) {
+                    // 로그인 성공 -> json -> Object 순서로 변환
+                    // 초기화 때 json으로 받는다해서 변경해줘야한다
+                    const userInfo = this.response;
+                    loginSuccess(userInfo)
+
+                    /* // login success 처리..
+                    console.log("userInfo", JSON.stringify(userInfo));
+                    //display:block -> display:none;
+                    // block이 보여주는 거고 none이 아무것도 안 보여주는거
+                    const loginWrapper = document.querySelector('#login-wrapper');
+                    loginWrapper.setAttribute("style", "display:none;");
+
+                    //display:none -> display:block;
+                    const loginSuccess = document.querySelector("#login-success");
+                    loginSuccess.setAttribute("style", "display:block;");
+
+                    // 로그인 성공 후 값 표시
+                    // div - ul - li 속 id값을 넣으면 된다 
+                    const loginUserId = document.getElementById("login-userId");
+                    const loginUserName = document.getElementById("login-userName");
+                    const loginCartId = document.getElementById("login-cartId");
+
+                    // span 안에 값 넣기 설정
+                    loginUserId.innerText = userInfo.userId;
+                    loginUserName.innerText = userInfo.userName;
+                    loginCartId.innerText = userInfo.cartId; */
                 }
             });
             
